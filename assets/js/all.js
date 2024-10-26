a720ce60b8359f4575bda38eeea9689cc83f1f14
AlfieTerryyy <168709104+AlfieTerryyy@users.noreply.github.com>
2024-10-26 21:48:33 +0100

    Update all.js

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,6 @@
 import 'https://alfieterry.co.uk/assets/js/pounder2.js';
 import 'https://alfieterry.co.uk/assets/js/nav.js';
 
+<script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/2da8fc4dc8652b6cbb405dc2f6d68675.js"></script>
+
 // Any additional code can go here
