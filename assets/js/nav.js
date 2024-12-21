944fb8ba757cc76108cf8fa87208193bddcda6e8
AlfieTerryyy <168709104+AlfieTerryyy@users.noreply.github.com>
2024-12-21 15:27:01 +0000

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -51,3 +51,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+    let lastScrollTop = 0;
+    const header = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            header.classList.add('hidden');
+        } else {
+            header.classList.remove('hidden');
+        }
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Prevent negative values
+    });
