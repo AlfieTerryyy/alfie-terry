fc0512d193e73a0fd77da6b4ecc57dd64aa5d75c
AlfieTerryyy <168709104+AlfieTerryyy@users.noreply.github.com>
2025-01-10 22:07:48 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,15 +59,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-// LOAD -----------------------------------------------------------------------------------------------------
-// Set the duration you want for the spinner to stay visible (e.g., 2 minutes = 120000 ms)
-const spinnerDuration = 1200; // 2 minutes
-
-// Show the spinner initially
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    const spinner = document.getElementById('loading-spinner');
-    spinner.style.display = 'none';
-  }, spinnerDuration);
-});
